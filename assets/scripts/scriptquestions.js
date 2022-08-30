e2ca430bc04bcbd5dde39607aed62bbfa580c51c
Madson_Silva <madsonsilva2005@gmail.com>
2022-08-29 21:44:36 -0300

    corrigido os erros do script

--- assets/scripts/scriptquestions.js
+++ assets/scripts/scriptquestions.js
@@ -1,15 +1,17 @@
 let nivel = 5;
-let listaNivel = []
+let listaNivel;
+
+
+console.log(list[0])
 
 function questions(){
+
+
+
+
     let listaDeQuestoes = [
         
-
-
-
-
-
-        Item1 = {
+        {
             numero : 2,
             enunciado: "A fórmula estrutural da metanamina é:",
             alternativas : ["CH3—NH2",
@@ -25,7 +27,7 @@ function questions(){
         },
 
 
-        Item2 = {
+        {
             numero : 2,
             enunciado : `Nome da fórmula estrutural abaixo:
             
@@ -40,21 +42,27 @@ function questions(){
             resposta: 3,
             nivel: 5,
         },
-        item3 = {
+
+
+        {
             numero: 3,
             enunciado: "(PUC-RJ) A seguir está representada a estrutura da dihidrocapsaicina, uma substância comumente encontrada em pimentas e pimentões. A seguir está representada a estrutura da dihidrocapsaicina, uma substância comumente encontrada em pimentas e pimentões. Na dihidrocapsaicina, está presente, entre outras, a função orgânica:",
             alternativas: ["Álcool.", "Amina.", "Amida.", "Éster."],
             resposta: 3,
             nivel: 5,
         },
-        item4 = {
+
+
+        {
             numero: 4,
             enunciado: "(PUC-MG) O princípio ativo dos analgésicos comercializados com nomes de Tylenol, Cibalena, Resprin é o paracetamol, cuja fórmula está representada a seguir. O princípio ativo dos analgésicos comercializados com nomes de Tylenol, Cibalena, Resprin é o paracetamol, cuja fórmula está representada a seguir",
             alternativas: ["Fenol, cetona e amina.", "Álcool, cetona e amina.", "Álcool e amida.", "Fenol e amida."],
             resposta: 4,
             nivel: 5,
         },
-        item5 = {
+        
+        
+        {
             numero: 5,
             enunciado: "A camada de ozônio (cinturão de Van Allen), que protege a biosfera do efeito nocivo de certas radiações, sofre ataque destrutivo dos CFC (clorofluorcarbono) usados em aerossóis e refrigeração, por exemplo. O mais usado, freon-12, de fórmula molecular CF2Cℓ2, pertence à função:",
             alternativas: ["a)hidrocarboneto", "cloreto de ácido", "haleto orgânico.", "hidrocarboneto insaturado."],
@@ -62,7 +70,9 @@ function questions(){
             nivel: 5,
         },
 
-        item6 = {
+        
+        
+        {
             numero: 6,
             enunciado: "Hidrocarbonetos halogenados, usados em aerossóis, são também responsáveis pela destruição da camada de ozônio da estratosfera. São exemplos de hidrocarbonetos halogenados:",
             alternativas: ["CH2Cℓ2 e CH3CH3","CH3COCℓ e CH3OCH3Cℓ", "CFCℓ3 e CHCℓ3.", "CH3NH2 e CFCℓ3."],
@@ -70,14 +80,19 @@ function questions(){
             nivel: 6,
         },
 
-        item7 = {
+        
+        
+        {
             numero: 7,
             enunciado: "(Unitau) O composto: O composto apresenta radicais que caracterizam Apresenta radicais que caracterizam:",
             alternativas: ["Álcool e aminoácido.", "Fenol e ácido.", "Álcool e amina.", "Álcool e amida."],
             resposta: 3,
             nivel: 6,
         },
-        item8 = {
+        
+        
+        
+        {
             numero: 8,
             enunciado: "(Unitau) O composto: O composto apresenta radicais que caracterizam Apresenta radicais que caracterizam:",
             alternativas: ["Álcool e aminoácido.", "Fenol e ácido.", "Álcool e amina.", "Álcool e amida."],
@@ -85,7 +100,10 @@ function questions(){
             nivel: 6,
         }, 
 
-        item9 = {
+        
+        
+        
+        {
             numero: 9,
             enunciado: "(Fuvest – SP) Hidrocarbonetos halogenados, usados em aerossóis, são também responsáveis pela destruição da camada de ozônio da estratosfera. São exemplos de hidrocarbonetos halogenados:",
             alternativas: ["CH2Cℓ2 e CH3CH3", "CH3COCℓ e CH3OCH2Cℓ", "CFCℓ3 e CHCℓ3", "CH3NH2 e CFCℓ3","CH3CHFCℓ e CH3COCℓ"],
@@ -93,7 +111,10 @@ function questions(){
             nivel: 6,
         },
 
-        item10 = {
+        
+        
+        
+        {
             numero: 10,
             enunciado: "Em compostos orgânicos ou inorgânicos o átomo de oxigênio pode ser substituído por um átomo de enxofre, ambos pertencentes à mesma família da Tabela Periódica realizando, assim, a mesma quantidade de ligações. O etanol, por exemplo, pode formar o etanotiol, um tio-álcool. Como se pode obter um tio-éter a partir do éter etílico?",
             alternativas: ["CH3CH2SH.", "CH3OCH3.", "CH3SCH3.", "CH3CH2OCH2CH3.", "CH3CH2SCH2CH3."],
@@ -114,8 +135,9 @@ function separador() {
 
         let numeroQ = Math.floor(Math.random() * lista.length);
         questao = lista[numeroQ];
+        
 
-        if(questao.nivel == nivel){
+        if(questao.nivel == nivel && !lista_level.includes(questao)){
             lista_level.push(questao)
             qtdeQuestions++;
         }
@@ -128,22 +150,26 @@ function separador() {
 function pegaQuestion(lista){
     listaNivel = lista
     let questao;
-    
-    console.log("odi")
+   
     let numeroQ = Math.floor(Math.random() * listaNivel.length);
     questao = listaNivel[numeroQ];
-    listaNivel.splice(questao);
-    console.log(lista)
+    listaNivel.pop(listaNivel[questao]);
+
+    nivel = 4;
+
+
         
 
 
     return questao;
 }
 
-if(listaNivel.length == 0){
-    listaNivel = separador();
-    console.log(pegaQuestion(listaNivel));
-}
+
+
+console.log(pegaQuestion(separador()))
+console.log(listaNivel)
+
+
 
 
 
